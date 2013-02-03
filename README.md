7d6191c7ace0666fab2169538792eca92af7cd57
HPNeo <hpneo@hotmail.com>
2013-02-03 15:20:26 -0500

    Add README and support for exceptions

---
+++ README.md
@@ -0,0 +1,3 @@
+Consola.js
+==========
+Consola is a JavaScript console inside browser which allows test your code and inspect objects.
--- consola.js
+++ consola.js
@@ -19,7 +19,13 @@ window.onload = function() {
   consola_form.onsubmit = function(e) {
     e.preventDefault();
 
-    var results = (new Function(editor.getValue()))();
+    var results;
+
+    try {
+      results = (new Function(editor.getValue()))();
+    } catch(error) {
+      results.lineNumber = (error.lineNumber - 24) || parseInt(error.stack.split('\n')[1].split(',').pop().replace(')', '').split(':')[1]) - 1;
+    }
 
     if(results instanceof Array) {
       var results_as_array = [], results_as_string = "";
@@ -36,8 +42,11 @@ window.onload = function() {
     else if(results instanceof Date) {
       results_as_string = results;
     }
+    else if(results instanceof Error) {
+      results_as_string = results.name + ' : ' + results.message + ' (at line ' + results.lineNumber + ')';
+    }
     else if(results instanceof Object) {
-      var results_as_array = [], results_as_string = "";
+      var results_as_array = [], results_as_string = "", i;
 
       for(i in results) {
         results_as_array.push(i + ' : ' + results[i]);
@@ -49,8 +58,12 @@ window.onload = function() {
       results_as_string = results;
     }
 
-    results_item = document.createElement('li');
+    var results_item = document.createElement('li');
     results_item.innerHTML = "<strong>></strong> " + results_as_string;
+
+    if(results instanceof Error) {
+      results_item.className = 'error';
+    }
 
     console.log(results, results_as_string)
 
